54bb5ed80c77e4b1ba3c618eb11f31e74150723e
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2022-02-23 21:43:30 +0100

    /

--- Space Batttlz/Assets/script/player/player.cs
+++ Space Batttlz/Assets/script/player/player.cs
@@ -36,11 +36,11 @@ public class player : MonoBehaviour
 
         // if( direction.magnitude >= 0.1f){
 
-            // float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            // float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            // transform.rotation = Quaternion.Euler(0f, angle, 0f); 
+        //     float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        //     float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        //     transform.rotation = Quaternion.Euler(0f, angle, 0f); 
 
-            // controller.Move(direction * speed * Time.deltaTime);
+        //     controller.Move(direction * speed * Time.deltaTime);
         // }
 
     }
