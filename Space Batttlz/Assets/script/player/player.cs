12f803b668b7f254fdc5c6c0f45d93dc71565e89
chavantfou <73283488+LBROCHARD@users.noreply.github.com>
2022-03-14 23:15:31 +0100

    [player.cs, cameraMovement.cs, SampleScene.unity] feat: correct camera follow player

--- Space Batttlz/Assets/script/player/player.cs
+++ Space Batttlz/Assets/script/player/player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     // référence au vaisseau
-    public GameObject playerObject;
+    // public GameObject playerObject;
     // référence à un mousePositionObject
     public GameObject mousePositionObject;
 
@@ -32,24 +32,11 @@ public class player : MonoBehaviour
     {
         // ---- rotation vers mousePositionObject ----
 
-        // Vector3 qui correspond à la direction en prenant la position du "mousePositionObject" et soustrait la position du player
-        //Vector3 direction = mousePositionObject.transform.position - transform.position;
-        //Debug.Log("direction =" + direction);
-        // renvoie une rotation créée par le devant actuel et la direction a pointer
-        //Quaternion toRotation = Quaternion.LookRotation(transform.forward, direction);
-        // Debug.Log( "dir =" + direction + "forwrd =" + transform.forward + "toRotation =" + toRotation);
-        // /!\ en chantier /!\ mais grosso modo ça tourne le joueur vers la rotation créée plus haut
-        //transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, 1 * Time.deltaTime);
-        //transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 1 * Time.deltaTime);
-        //transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, 100f, 0.0f), 1 * Time.deltaTime);
-
         // calcule la différence entre la position du "mousePositionObject" et du player
-
-        Quaternion toRotation = Quaternion.LookRotation(mousePositionObject.transform.position - playerObject.transform.position);
-        //toRotation.Set(0, toRotation.y, toRotation.z, toRotation.w);
+        Quaternion toRotation = Quaternion.LookRotation(mousePositionObject.transform.position - transform.position);
 
         // tourne vers "toRotation"
-        playerObject.transform.rotation = Quaternion.RotateTowards(playerObject.transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
 
         //ligne qui fonctionne pour se tourner d'un coup vers l'objet
         //transform.LookAt(mousePositionObject.transform);
@@ -61,11 +48,10 @@ public class player : MonoBehaviour
         //transform.position = Vector3.MoveTowards(transform.position, mousePositionObject.transform.position, (speed / 10) );
         
         // create a forward vector
-        // Vector3 forward = playerObject.transform.position + playerObject.transform.forward;
-        Vector3 forward = transform.position + playerObject.transform.forward;
-        Vector3 betterForward = new Vector3(forward.x, 10f, forward.z);
+        Vector3 forward = transform.position + transform.forward;
+        Vector3 betterForward = new Vector3(forward.x, 2f, forward.z);
         // Debug.Log( "forward =" + forward );
-        Debug.Log( "betterForward =" + betterForward );
+        // Debug.Log( "betterForward =" + betterForward );
 
 
         // ligne pour le déplacement modifié
